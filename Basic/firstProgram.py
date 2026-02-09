645293275ba9d927640782ecdef220335d1047ba
Om Kumar <omku415@gmail.com>
2026-02-10 00:34:06 +0530

    first program and variables .

---
+++ Basic/firstProgram.py
@@ -0,0 +1,16 @@
+print("Hello World")
+#This print on the same line .
+print("Hi,","How are You?")
+
+# variables
+name= "om"
+age=23
+
+print("My name is:",name)
+
+print("My age is:",age)
+
+# to print the type of the variable.
+print(type(name))
+print(type(age))
+
