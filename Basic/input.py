09b13a88d479ce3875fd888b9b9fd2eb73d15c70
Om Kumar <omku415@gmail.com>
2026-02-13 00:31:45 +0530

    Add basic input handling and type conversion examples

---
+++ Basic/input.py
@@ -0,0 +1,13 @@
+
+# Taking input in the python
+# by default all the input will be string .
+name=input("Enter your name")
+
+print (name)
+
+# if we want to take the integer input we have to parse it into integer
+
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+
+print(num1+num2)
---
+++ Basic/typeConersionAndCasting.py
@@ -0,0 +1,13 @@
+#type conversion -> it is done automatically by python..
+a=2
+b=4.4
+# int value will bbe automatically converted into float as it is superior from int
+print(a+b)
+
+#Type casting 
+
+a="2"
+b=3
+# here we are manually converting the string to the intger through the type casting.
+sum = b+ int(a)
+print(sum)
