182e2613ab5e49cc9ade6e613591a2864c3b448e
Om Kumar <omku415@gmail.com>
2026-02-17 23:31:20 +0530

    Add string manipulation examples including newline, tab, concatenation, and length

---
+++ Basic/string.py
@@ -0,0 +1,17 @@
+#for the next line
+str1= "Hey\nGood Night" 
+# for the tab sequence
+str2= "Hey\t Good Night"
+
+print(str1)
+print(str2)
+
+#concatination
+str3="om"
+str4="kumar"
+
+print(str3+str4)
+
+#length
+
+print(len(str1))
