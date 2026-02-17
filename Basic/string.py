1a3b7867b2ac885a6e676131d333eed3e3f1469a
Om Kumar <omku415@gmail.com>
2026-02-18 00:10:48 +0530

    Add string slicing examples and negative indexing to string.py; include StringFunctions.png image

--- Basic/string.py
+++ Basic/string.py
@@ -15,3 +15,13 @@ print(str3+str4)
 #length
 
 print(len(str1))
+
+#slicing
+print(str4[1:3])# ending index is not included
+
+print(str4[0:])# if we  miss the last index it means to end
+print(str4[:4])# if we  miss the starting index it means from 0.
+
+# in pyton there is also a feature of negative indexing 
+# it will start from end with -1 then -2 etc
+print(str4[-3:-1])
