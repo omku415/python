6f7455064c911c19dcc6fec1cb5da54535fc3ba3
Om Kumar <omku415@gmail.com>
2026-02-22 01:29:54 +0530

    Add conditional statements example in conditional.py

---
+++ Basic/conditional.py
@@ -0,0 +1,10 @@
+age = 0
+
+if(age>=18):
+    print("can vote and drive")
+elif(age>0&age<18):
+    print("please have lisence")
+else:
+    print("you does not belong to this world")    
+
+  # the space given in python is called indentation.  
